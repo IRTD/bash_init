b920bac2104e9ebde1231f81a82fae31e07439df
IRTD <reisbaellchen17@gmail.com>
2023-08-05 22:18:28 +0200

    Fixed shopt add to bashrc and sourced .bashrc before running cargo installs

--- full_install.sh
+++ full_install.sh
@@ -13,8 +13,8 @@ apt install kitty -y
 
 # Create kitty config dir and download conf files
 mkdir ~/.config/kitty/
-curl https://raw.githubusercontent.com/SolDoesTech/hyprland/main/kitty/kitty.conf -o ~/.config/kitty/kitty.conf
-curl https://raw.githubusercontent.com/SolDoesTech/hyprland/main/kitty/mocha.conf -o ~/.config/kitty/mocha.conf
+curl https://raw.githubusercontent.com/SolDoesTech/hyprland/main/kitty/kitty.conf > ~/.config/kitty/kitty.conf
+curl https://raw.githubusercontent.com/SolDoesTech/hyprland/main/kitty/mocha.conf > ~/.config/kitty/mocha.conf
 
 # Add build-essential and cmake
 apt install build-essential -y
@@ -23,6 +23,8 @@ apt install cmake -y
 # Install rust and cargo
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+source ~/.bashrc
+
 # Install programs with cargo
 cargo install -q exa &
 cargo install -q zoxide &
@@ -30,7 +32,7 @@ cargo install -q starship &
 cargo install -q bat &
 
 # Install starship config
-curl https://raw.githubusercontent.com/IRTD/helix-config/master/starship.toml -o ~/.config/starship.toml
+curl https://raw.githubusercontent.com/IRTD/helix-config/master/starship.toml > ~/.config/starship.toml
 
 # Add starship eval
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
@@ -39,7 +41,7 @@ echo 'eval "$(starship init bash)"' >> ~/.bashrc
 echo 'eval "$(zoxide init bash)"' >> ~/.bashrc
 
 # Add auto-cd
-shopt -s autocd
+echo "shopt -s autocd" >> ~/.bashrc
 
 # Add custom keybinds to .bashrc
 echo "alias .='exa -1al'" >> ~/.bashrc
